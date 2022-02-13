022156c361ca43c1518351122752ecbff4efc859
casanoir <mendoun@hotmail.de>
2022-02-13 02:53:07 +0100

    create plant dilog component

--- src/app/dashboard/dashboard/components/plant-dialog/plant-dialog.component.ts
+++ src/app/dashboard/dashboard/components/plant-dialog/plant-dialog.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { PlantModule } from 'src/app/modules/plant.module';
+import { ApiService } from 'src/app/service/api.service';
 
 @Component({
   selector: 'app-plant-dialog',
   templateUrl: './plant-dialog.component.html',
-  styleUrls: ['./plant-dialog.component.scss']
+  styleUrls: ['./plant-dialog.component.scss'],
 })
-export class PlantDialogComponent implements OnInit {
+export class PlantDialogComponent {
+  recieveId: any;
+  plant$!: PlantModule[];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private api: ApiService,
+    private dialogRef: MatDialogRef<PlantDialogComponent>,
+    // @Inject(MAT_DIALOG_DATA) public data: any
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {
+    this.recieveId = data.id;
+    this.api.showPlant(this.recieveId).subscribe((data) => {
+      this.plant$ = data;
+      console.log(this.plant$);
+    });
   }
 
+  close() {
+    this.dialogRef.close();
+  }
 }
